82cdce556a3b58ab8ad48763985d45b55d8c4dc1
Chandrashekhar Naik <chandrashekhar.naik@legal.one>
2023-03-07 18:03:15 +0530

    refactor methods

--- plugin-flex-as-iframe/src/FlexAsIframePlugin.tsx
+++ plugin-flex-as-iframe/src/FlexAsIframePlugin.tsx
@@ -1,57 +1,80 @@
-import * as Flex from '@twilio/flex-ui';
-import { FlexPlugin } from '@twilio/flex-plugin';
+import * as Flex from "@twilio/flex-ui";
+import { FlexPlugin } from "@twilio/flex-plugin";
 
-const PLUGIN_NAME = 'FlexAsIframePlugin';
+const PLUGIN_NAME = "FlexAsIframePlugin";
+const TASK_CHANNEL = {
+  VOICE: "voice",
+};
 
 export default class FlexAsIframePlugin extends FlexPlugin {
   constructor() {
     super(PLUGIN_NAME);
   }
 
-  /**
-   * This code is run when your plugin is being started
-   * Use this to modify any UI components or attach to the actions framework
-   *
-   * @param flex { typeof Flex }
-   */
   async init(flex: typeof Flex, manager: Flex.Manager): Promise<void> {
-
-    // Flex in an iFrame
     if (window.self !== window.top) {
-      //Hide CRM container since Flex is in iFrame
       flex.AgentDesktopView.defaultProps.showPanel2 = false;
-      // Define a function for what to do when a message from postMessage() comes in
-      const receiveMessage = (event : any) => {
-        // Invoke the Flex Outbound Call Action
-        flex.Actions.invokeAction("StartOutboundCall", {destination: event.data});
-      }
 
-      // Add an event listener to associate the postMessage() data with the receiveMessage, to trigger an outbound call
+      const receiveMessage = (event: any) => {
+        let actionName = event.data.actionName;
+        let options;
+
+        switch (actionName) {
+          case "StartOutboundCall":
+            options = {
+              destination: event.data.attributes.to,
+            };
+            break;
+          default:
+            console.log("unknown event:", event.data.action);
+        }
+        //invoke action
+        flex.Actions.invokeAction(actionName, options);
+      };
+
       window.addEventListener("message", receiveMessage);
     }
 
     flex.Actions.replaceAction("AcceptTask", (payload, original) => {
       return new Promise<void>((resolve, reject) => {
-        console.log("sending postmessage after AcceptTask");
-        //in this POC we use the inbound from attribute as the unique identifier.
-        if(payload.task.attributes.direction == 'inbound'){
-          parent.postMessage(`{"pop":"${payload.task.attributes.from}"}`,'*');
+        let data;
+        if (payload.task.taskChannelUniqueName === TASK_CHANNEL.VOICE) {
+          switch (payload.task.attributes.direction) {
+            case "inbound":
+              data = {
+                attributes: payload.task.attributes,
+              };
+              break;
+            case "outbound":
+              data = {
+                attributes: payload.task.attributes,
+              };
+              break;
+            default:
+              console.log("Unhandled replaceAction::AcceptTask");
+          }
+
+          parent.postMessage(JSON.stringify(data), "*");
         }
         resolve();
       }).then(() => original(payload));
     });
 
-    //send message to iframe parent, in this case the dummy CRM
     flex.Actions.replaceAction("HangupCall", (payload, original) => {
       return new Promise<void>((resolve, reject) => {
-          console.log("sending postmessage after HangupCall");
-          let to = payload.task.attributes.to;
-          //if it is outbound, to will be undefined so set it to outbound
-          if(typeof to == 'undefined'){
-            to = payload.task.attributes.outbound_to;
-          }
-          parent.postMessage(`{"from": "${payload.task.attributes.from}", "to":"${to}", "direction":"${payload.task.attributes.direction}", "duration":"${payload.task.age}", "agent":"${manager.user.identity}"}`,'*');
-          resolve();
+        //for outbound calls, to will be undefined
+        let { from, to, outbound_to, direction } = payload.task.attributes;
+
+        let data = {
+          from,
+          to,
+          outbound_to,
+          direction,
+          duration: payload.task.age,
+          agent: manager.user.identity,
+        };
+        parent.postMessage(JSON.stringify(data), "*");
+        resolve();
       }).then(() => original(payload));
     });
   }
